bb064e2d0f183d745497eba5a48e01b5339c05f4
Matias Salimbene <git config --global -e matias.salimbene@gmail.com>
2024-02-03 10:16:50 -0300

    kick off

--- App.js
+++ App.js
@@ -1,11 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, SafeAreaView, View } from "react-native";
+import { useWindowDimensions } from "react-native";
+import { useDeviceOrientation } from "@react-native-community/hooks";
 
 export default function App() {
+  console.log(useWindowDimensions());
+  console.log(useDeviceOrientation());
+  const landscape = useDeviceOrientation() === "landscape";
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View
+      style={{
+        backgroundColor: "white",
+        flex: 1,
+        flexDirection: "row",
+      }}
+    >
+      <View
+        style={{ backgroundColor: "dodgerblue", width: 100, height: 100 }}
+      />
+      <View style={{ backgroundColor: "gold", width: 100, height: 100 }} />
+      <View style={{ backgroundColor: "tomato", width: 100, height: 100 }} />
     </View>
   );
 }
@@ -13,8 +27,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    // alignItems: "center",
+    // justifyContent: "center",
   },
 });
