41bf945fb174c788436f8d43820d9fcc6b497e0c
Matias Salimbene <git config --global -e matias.salimbene@gmail.com>
2024-02-03 12:05:58 -0300

    stylinh

--- App.js
+++ App.js
@@ -1,34 +1,11 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, SafeAreaView, View } from "react-native";
-import { useWindowDimensions } from "react-native";
-import { useDeviceOrientation } from "@react-native-community/hooks";
+import { View } from 'react-native';
+
+import AppText from './app/components/AppText';
 
 export default function App() {
-  console.log(useWindowDimensions());
-  console.log(useDeviceOrientation());
-  const landscape = useDeviceOrientation() === "landscape";
   return (
-    <View
-      style={{
-        backgroundColor: "white",
-        flex: 1,
-        flexDirection: "row",
-      }}
-    >
-      <View
-        style={{ backgroundColor: "dodgerblue", width: 100, height: 100 }}
-      />
-      <View style={{ backgroundColor: "gold", width: 100, height: 100 }} />
-      <View style={{ backgroundColor: "tomato", width: 100, height: 100 }} />
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <AppText> I love React Native! </AppText>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    // alignItems: "center",
-    // justifyContent: "center",
-  },
-});
