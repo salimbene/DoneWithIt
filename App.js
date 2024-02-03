cde9d6051caa6d9518403810bd92565b4e5503f5
Matias Salimbene <git config --global -e matias.salimbene@gmail.com>
2024-02-03 18:51:33 -0300

    Update imports and remove unused code

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
-import { View } from 'react-native';
+import React from 'react';
+// import { View } from 'react-native';
 
-import AppText from './app/components/AppText';
+// import AppText from './app/components/AppText';
+// import AppButton from './app/components/AppButton';
+// import WelcomeScreen from './app/screens/WelcomeScreen';
+
+// import Card from './app/components/Card';
+// import ListingDetailsScreen from './app/screens/ListingDetailsScreen';
+// import ViewImageScreen from './app/screens/ViewImageScreen';
+import MessagesScreen from './app/screens/MessagesScreen';
 
 export default function App() {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <AppText> I love React Native! </AppText>
-    </View>
-  );
+  return <MessagesScreen />;
 }
