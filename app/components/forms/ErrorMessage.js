d0d4a0f7dfd652efa96e0fa1ceca6eec729f7d57
Matias Salimbene <git config --global -e matias.salimbene@gmail.com>
2024-02-05 10:32:08 -0300

    Add ESLint and update LoginScreen component

--- app/components/forms/ErrorMessage.js
+++ app/components/forms/ErrorMessage.js
@@ -1,9 +1,9 @@
-import { StyleSheet, Text, View } from 'react-native';
 import React from 'react';
-import AppText from './AppText';
+import { StyleSheet } from 'react-native';
+import AppText from '../AppText';
 
-const ErrorMessage = ({ error }) => {
-  if (!error) return null;
+const ErrorMessage = ({ error, visible }) => {
+  if (!visible || !error) return null;
   return <AppText style={styles.error}>{error}</AppText>;
 };
 
